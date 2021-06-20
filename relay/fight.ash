81d0d069aa64f1bd0976fa2fd2a6ff9fdb390d32
Sibusten <Sibusten@gmail.com>
2021-06-20 04:30:43 -0400

    Make indentation consistent and use same-line braces

--- relay/fight.ash
+++ relay/fight.ash
@@ -1,16 +1,16 @@
 buffer addMultiCAB( buffer result ) {
-  matcher m = create_matcher( "actionbar\\.\\d+\\.js.+?<\\/script>" , result );
-  if ( !m.find() ) return result;
-  result.insert( m.end() , "<script src='MultiCAB.js'></script>" );
-  return result;
+    matcher m = create_matcher( "actionbar\\.\\d+\\.js.+?<\\/script>" , result );
+    if ( !m.find() ) return result;
+    result.insert( m.end() , "<script src='MultiCAB.js'></script>" );
+    return result;
 }
 
-void main() {  
-	buffer result = visit_url();
-  result = result.addMultiCAB();
-  
-  // Add doctype to escape quirks mode
-	result.replace_string('<html>', '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\n<html>');
-  
-  result.write();
+void main() {
+    buffer result = visit_url();
+    result = result.addMultiCAB();
+
+    // Add doctype to escape quirks mode
+    result.replace_string('<html>', '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\n<html>');
+
+    result.write();
 }
--- relay/multicab.js
+++ relay/multicab.js
@@ -1,142 +1,134 @@
-function getCache(buttonraw) // replaces function of same name in actionbar.js
-{
-  var button = ( buttonraw - 1 ) % 12 + 1; // NEW LINE
-  var modpage = ( Math.floor( ( buttonraw - 1 ) / 12 ) + top.buttoncache.whichpage ) % 12; // NEW LINE
-	var nullcache = {type: null, id: null, pic: null};
-	if (button < 1 || button > 12)
-		return nullcache;
-	if (state["shapeshift"])
-	{
-		var obj = shiftbar[button - 1];
-		if (obj == null)
-			return nullcache;
-		else
-		{
-			if (typeof state[obj["type"] + "s"][obj["id"]] == "undefined")
-				return nullcache;
+// replaces function of same name in actionbar.js
+function getCache(buttonraw) {
+    var button = (buttonraw - 1) % 12 + 1; // NEW LINE
+    var modpage = (Math.floor((buttonraw - 1) / 12) + top.buttoncache.whichpage) % 12; // NEW LINE
+    var nullcache = { type: null, id: null, pic: null };
+    if (button < 1 || button > 12)
+        return nullcache;
+    if (state["shapeshift"]) {
+        var obj = shiftbar[button - 1];
+        if (obj == null)
+            return nullcache;
+        else {
+            if (typeof state[obj["type"] + "s"][obj["id"]] == "undefined")
+                return nullcache;
 
-			obj["pic"] = state[obj["type"] + "s"][obj["id"]]["pic"];
-			return obj;
-		}
-	}
-	else
-		with (top.buttoncache)
-		{
-			var obj = pages[modpage][button - 1];  // MODIFIED LINE
-			if (obj == null)
-				return nullcache;
-			else
-				return obj;
-		}
+            obj["pic"] = state[obj["type"] + "s"][obj["id"]]["pic"];
+            return obj;
+        }
+    }
+    else
+        with (top.buttoncache) {
+            var obj = pages[modpage][button - 1];  // MODIFIED LINE
+            if (obj == null)
+                return nullcache;
+            else
+                return obj;
+        }
 }
 
-function setCache(buttonraw, type, id, pic) // replaces function of same name in actionbar.js
-{
-  var button = ( buttonraw - 1 ) % 12 + 1; // NEW LINE
-  var modpage = ( Math.floor( ( buttonraw - 1 ) / 12 ) + top.buttoncache.whichpage ) % 12; // NEW LINE
-	var nullcache = {type: null, id: null, pic: null};
-	if (button < 1 || button > 12)
-		return null;
-	if (state["shapeshift"])
-		return null;
-	with (top.buttoncache)
-	{
-		if (type == null && id == null && pic == "blank")
-			pages[modpage][button - 1] = null;  // MODIFIED LINE
-		else
-		{
-			if (type == "action" && id == "attack")
-				pic = state.actions.attack.pic;
-			pages[modpage][button - 1] = {type: type, id: id, pic: pic};  // MODIFIED LINE
-		}
-	}
+// replaces function of same name in actionbar.js
+function setCache(buttonraw, type, id, pic) {
+    var button = (buttonraw - 1) % 12 + 1; // NEW LINE
+    var modpage = (Math.floor((buttonraw - 1) / 12) + top.buttoncache.whichpage) % 12; // NEW LINE
+    var nullcache = { type: null, id: null, pic: null };
+    if (button < 1 || button > 12)
+        return null;
+    if (state["shapeshift"])
+        return null;
+    with (top.buttoncache) {
+        if (type == null && id == null && pic == "blank")
+            pages[modpage][button - 1] = null;  // MODIFIED LINE
+        else {
+            if (type == "action" && id == "attack")
+                pic = state.actions.attack.pic;
+            pages[modpage][button - 1] = { type: type, id: id, pic: pic };  // MODIFIED LINE
+        }
+    }
 }
 
-function setPage(page) // replaces function of same name in actionbar.js
-{
-	var img;
-	if (page < 0 || page > 11)
-		return;
-	if (state["shapeshift"])
-		page = 0;
-	else
-		top.buttoncache.whichpage = page;
+// replaces function of same name in actionbar.js
+function setPage(page) {
+    var img;
+    if (page < 0 || page > 11)
+        return;
+    if (state["shapeshift"])
+        page = 0;
+    else
+        top.buttoncache.whichpage = page;
 
-	for (var i = 1; i <= state["totalbuttons"]; i++) // MODIFIED LINE
-	{
-		var cache = getCache(i);
-		var button = document.getElementById('button' + i);
-		if (cache)
-			assignButton(button, cache["type"], cache["id"], cache["pic"]);
-		else
-			unassignButton(button);
-	}
+    for (var i = 1; i <= state["totalbuttons"]; i++) { // MODIFIED LINE
+        var cache = getCache(i);
+        var button = document.getElementById('button' + i);
+        if (cache)
+            assignButton(button, cache["type"], cache["id"], cache["pic"]);
+        else
+            unassignButton(button);
+    }
 
-	if (state["funkslinging"])
-		for (var i = 1; i <= 2; i++)
-			if (img = state["ants" + i])
-				img.style.display = state["funkpage" + i] == whichpage ? "inline" : "none";
+    if (state["funkslinging"])
+        for (var i = 1; i <= 2; i++)
+            if (img = state["ants" + i])
+                img.style.display = state["funkpage" + i] == whichpage ? "inline" : "none";
 
-	var pageout = document.getElementById('page_out');
-	pageout.innerHTML = page + 1;
+    var pageout = document.getElementById('page_out');
+    pageout.innerHTML = page + 1;
 }
 
-function addElement( parent , type , className , id ) {
-  var el = document.createElement( type );
-  if ( className != undefined ) el.className = className;
-  if ( id ) el.id = id;
-  parent.appendChild( el );
-  return el;
+function addElement(parent, type, className, id) {
+    var el = document.createElement(type);
+    if (className != undefined) el.className = className;
+    if (id) el.id = id;
+    parent.appendChild(el);
+    return el;
 }
 
 function multiCAB_init() {
-  state.totalbuttons = 24;
-  var newtopbarheight = 110 + 'px';
-  var topbar = document.getElementById('topbar');
-  topbar.style.height = newtopbarheight;
-  document.getElementById('content_').style.top = newtopbarheight;
-  
-  var actionbar = document.getElementsByClassName('actionbar')[0];
-  var newbar = addElement( actionbar.firstChild , 'tr' , 'blueback');
-  var newqty = addElement( actionbar.firstChild , 'tr' , 'label' );
-  addElement( newbar , 'td' , 'spacer' ).colSpan = 2;
-  addElement( newqty , 'td' , 'spacer' ).colSpan = 2;
-  addElement( newbar , 'td' ).appendChild( document.getElementById('skills') );
-  addElement( newqty , 'td' ).innerHTML = 'skills';
-  with ( actionbar.firstChild ) {
-    for (var row = 0; row < 3; row++ ) {
-      children[row].children[2].className = 'spacer';
-      children[row].children[2].innerHTML = '';
+    state.totalbuttons = 24;
+    var newtopbarheight = 110 + 'px';
+    var topbar = document.getElementById('topbar');
+    topbar.style.height = newtopbarheight;
+    document.getElementById('content_').style.top = newtopbarheight;
+
+    var actionbar = document.getElementsByClassName('actionbar')[0];
+    var newbar = addElement(actionbar.firstChild, 'tr', 'blueback');
+    var newqty = addElement(actionbar.firstChild, 'tr', 'label');
+    addElement(newbar, 'td', 'spacer').colSpan = 2;
+    addElement(newqty, 'td', 'spacer').colSpan = 2;
+    addElement(newbar, 'td').appendChild(document.getElementById('skills'));
+    addElement(newqty, 'td').innerHTML = 'skills';
+    with (actionbar.firstChild) {
+        for (var row = 0; row < 3; row++) {
+            children[row].children[2].className = 'spacer';
+            children[row].children[2].innerHTML = '';
+        }
     }
-  }
-  addElement( newbar , 'td' , 'spacer' );
-  addElement( newqty , 'td' , 'spacer' );
-  
-  for ( var button_it = 1; button_it < 13; button_it++ ) {
-    button_id = button_it + 12;
-    state.buttonstate[ button_id ];
-    var newButtonTD = addElement( newbar , 'td' );
-    addElement( newqty , 'td' , '' , 'qty' + button_id ).style.height = '11px';
-    var newButton = addElement( newButtonTD , 'img' , '' , 'button' + button_id );
-    if (!state["shapeshift"])
-    {
-      dragdrop.addDrop(newButton);
-      dragdrop.register(newButton);
+    addElement(newbar, 'td', 'spacer');
+    addElement(newqty, 'td', 'spacer');
+
+    for (var button_it = 1; button_it < 13; button_it++) {
+        button_id = button_it + 12;
+        state.buttonstate[button_id];
+        var newButtonTD = addElement(newbar, 'td');
+        addElement(newqty, 'td', '', 'qty' + button_id).style.height = '11px';
+        var newButton = addElement(newButtonTD, 'img', '', 'button' + button_id);
+        if (!state["shapeshift"]) {
+            dragdrop.addDrop(newButton);
+            dragdrop.register(newButton);
+        }
+        newButton.onclick = buttonClick;
+        newButton.oncontextmenu = buttonClick;
     }
-    newButton.onclick = buttonClick;
-    newButton.oncontextmenu = buttonClick;
-  }
-  addElement( newbar , 'td' , 'spacer' );
-  addElement( newqty , 'td' , 'spacer' );
-  addElement( newbar , 'td' ).appendChild( document.getElementById('items') );
-  addElement( newqty , 'td' ).innerHTML = 'items';
-    with ( actionbar.firstChild ) {
-    for (var row = 0; row < 3; row++ ) {
-      children[row].children[18].className = 'spacer';
-      children[row].children[18].innerHTML = '';
+    addElement(newbar, 'td', 'spacer');
+    addElement(newqty, 'td', 'spacer');
+    addElement(newbar, 'td').appendChild(document.getElementById('items'));
+    addElement(newqty, 'td').innerHTML = 'items';
+    with (actionbar.firstChild) {
+        for (var row = 0; row < 3; row++) {
+            children[row].children[18].className = 'spacer';
+            children[row].children[18].innerHTML = '';
+        }
     }
-  }
-  
 }
 
 multiCAB_init();
